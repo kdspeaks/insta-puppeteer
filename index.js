670578367544d1e67f5ef9c1a12c94578f08972c
Kunal Dutta <kdutta494@gmail.com>
2021-09-13 15:26:24 +0530

    Initial Repo

---
+++ index.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs/promises')
+
+const script = async() => {
+    const browser = await puppeteer.launch({
+        headless: false
+    })
+    const page = await browser.newPage()
+    await page.goto('https://www.instagram.com/p/CTaD6wXsHSn/?__a=1', {waitUntil: "networkidle2"})
+    await page.waitFor(5000)
+    await page.content()
+
+    const res = await page.evaluate(() => {
+        return JSON.parse(document.querySelector('body').innerText)
+    })
+
+    let image = res.graphql.shortcode_media.display_url
+    await fs.writeFile("image.txt", image)
+
+
+}
+
+script()
